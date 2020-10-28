931bf8525e3da0d344d6e8f88ed0d08fc4a5c2c7
Adriana penha da silva <dry-k.ps@hotmail.com>
2020-10-28 20:43:15 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from '../src/components/header/header';
+import api from './api';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Row, Col } from 'react-bootstrap';
+ 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  state = {
+    moedas: [],
+  }
+  async componentDidMount() {
+    const response = await api.get('');
+    let moedasArray = [];
+    for (const moeda in response.data) {
+      moedasArray.push(response.data[moeda]);
+    }
+    console.log(moedasArray);
+    console.log('PASSOUs');
+
+    this.setState({ moedas: moedasArray });
+  }
+
+  render() {
+
+    const { moedas } = this.state;
+    return (
+      <div>
+        <Header />
+        <div className="App">
+        <br></br>
+        <Row className="justify-content-md-center">
+        <Col xs lg="8">
+        <Table striped bordered hover variant="primary" >
+        <thead>
+          <tr>
+            <th colSpan="3" className="text-left">Indices</th>
+          </tr>
+          { moedas.map(moeda => (
+                <tr>
+                  <th>{moeda.name}</th>
+                  <td>{moeda.pctChange}%</td>
+                  <td>R$ {moeda.ask}</td>
+                </tr>
+             
+        ))}
+         </thead>
+        </Table>
+        </Col>
+        </Row>
+       
+        </div>
+      </div>
+      
+    );
+  };
+};
 
 export default App;
